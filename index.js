cf09c0561aa80c4d5718d3b122040106fe95b578
patrick kisyula <pmusyimi42@gmail.com>
2022-08-28 13:26:33 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+//const { expect } = require("chai");
+
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  const p = document.querySelector("p")
+  p.textContent = 'This is really cool!'; 
+});  
+
+// function init(){
+//   const p = document.querySelector('p');
+//   p.innerText = "This is really cool!";
+//  };
+  
